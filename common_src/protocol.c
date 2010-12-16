ed755dfdf256e2c8c9c1bb4c2422ccc27a8d5616
Chungmin Lee <lemonedo@gmail.com>
2010-12-17 04:10:46 +0900

    implemented functions declared in protocol.h

---
+++ common_src/protocol.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "protocol.h"
+
+int
+parse_request(const char *str, struct request *req)
+{
+    size_t i, n, p;
+
+    n = strlen(str);
+    if (n > MAX_REQUEST_LEN - 1)
+        return -1;
+    if (strncmp(str, "GETA", 4) == 0)
+        req->method = GETA;
+    else if (strncmp(str, "RSUM", 4) == 0)
+        req->method = RSUM;
+    else if (strncmp(str, "SUSP", 4) == 0)
+        req->method = SUSP;
+    else if (strncmp(str, "PING", 4) == 0)
+        req->method = PING;
+    else if (strncmp(str, "INFO", 4) == 0)
+        req->method = INFO;
+    else if (strncmp(str, "NTFY", 4) == 0)
+        req->method = NTFY;
+    else
+        return -1;
+    req->has_uri = req->has_data = 0;
+    i = 4;
+    if (str[i] == '\n' && n == i + 1)  // no URI, no data
+        return 0;
+    if (str[i] == ' ') {  // has a URI
+        for (i = 5; i < n; ++i)
+            if (str[i] == '\n')
+                break;
+        if (i == n)
+            return -1;
+        p = i - 5;
+        if (p > MAX_URI_LEN - 1)
+            return -1;
+        strncpy(req->uri, str + 5, p);
+        req->uri[p] = 0;
+        req->has_uri = 1;
+        if (n == i)  // no data
+            return 0;
+    }
+    if (str[i + 1] != '\n' || str[n - 2] != '\n' || str[n - 1] != '\n')
+        return -1;
+    p = n - i - 4;
+    strncpy(req->data, str + i + 2, p);
+    req->data[p] = 0;
+    req->has_data = 1;
+    return 0;
+}
+
+int
+parse_response(const char *str, struct response *resp)
+{
+    size_t n, i, p;
+    int status;
+
+    n = strlen(str);
+    if (n > MAX_RESPONSE_LEN - 1)
+        return -1;
+    if (sscanf(str, "%d", &status) != 1)
+        return -1;
+    if (status < 100 || status > 999)
+        return -1;
+    resp->status = status;
+    i = 3;
+    if (str[i] != ' ')
+        return -1;
+    for (i = 4; i < n; ++i)
+        if (str[i] == '\n')
+            break;
+    if (i == n)
+        return -1;
+    p = i - 4;
+    if (p > MAX_MESSAGE_LEN - 1)
+        return -1;
+    strncpy(resp->message, str + 4, p);
+    resp->message[p] = 0;
+    resp->has_data = 0;
+    if (n == i)  // no data
+        return 0;
+    if (str[i + 1] != '\n' || str[n - 2] != '\n' || str[n - 1] != '\n')
+        return -1;
+    p = n - i - 4;
+    strncpy(resp->data, str + i + 2, p);
+    resp->data[p] = 0;
+    resp->has_data = 1;
+    return 0;
+}
+
+int
+serialize_request(const struct request *req, char *str)
+{
+    const char *m;
+    size_t i, ulen, dlen;
+
+    switch (req->method) {
+    case GETA: m = "GETA"; break;
+    case RSUM: m = "RSUM"; break;
+    case SUSP: m = "SUSP"; break;
+    case PING: m = "PING"; break;
+    case INFO: m = "INFO"; break;
+    case NTFY: m = "NTFY"; break;
+    default: return -1;
+    }
+    i = 0;
+    strncpy(str + i, m, 4); i += 4;
+    if (req->has_uri) {
+        str[i] = ' '; i += 1;
+        ulen = strlen(req->uri);
+        if (ulen > MAX_URI_LEN - 1)
+            return -1;
+        strncpy(str + i, req->uri, ulen); i += ulen;
+    }
+    str[i] = '\n'; i += 1;
+    if (req->has_data) {
+        str[i] = '\n'; i += 1;
+        dlen = strlen(req->data);
+        if (dlen > MAX_DATA_LEN - 1)
+            return -1;
+        strncpy(str + i, req->data, dlen); i += dlen;
+        str[i] = '\n'; i += 1;
+        str[i] = '\n'; i += 1;
+    }
+    str[i] = 0;
+    return 0;
+}
+
+int
+serialize_response(const struct response *resp, char *str)
+{
+    size_t i, mlen, dlen;
+
+    if (resp->status < 100 || resp->status > 999)
+        return -1;
+    i = 0;
+    i += sprintf(str, "%d", resp->status);
+    str[i] = ' '; i += 1;
+    mlen = strlen(resp->message);
+    if (mlen > MAX_MESSAGE_LEN - 1)
+        return -1;
+    strncpy(str + i, resp->message, mlen); i += mlen;
+    str[i] = '\n'; i += 1;
+    if (resp->has_data) {
+        str[i] = '\n'; i += 1;
+        dlen = strlen(resp->data);
+        if (dlen > MAX_DATA_LEN - 1)
+            return -1;
+        strncpy(str + i, resp->data, dlen); i += dlen;
+        str[i] = '\n'; i += 1;
+        str[i] = '\n'; i += 1;
+    }
+    str[i] = 0;
+    return 0;
+}
--- common_src/protocol.h
+++ common_src/protocol.h
@@ -1,14 +1,19 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-// The maximum lengths of requests and responses in the serialized form.
-#define MAX_REQUEST_LEN 586
-#define MAX_RESPONSE_LEN 585
+// including the null caracter
+#define MAX_URI_LEN 64
+#define MAX_MESSAGE_LEN 64
+#define MAX_DATA_LEN 512
+
+// The maximum lengths of requests and responses in the serialized form,
+// including the null character.
+#define MAX_REQUEST_LEN (MAX_URI_LEN + MAX_DATA_LEN + 8)
+#define MAX_RESPONSE_LEN (MAX_MESSAGE_LEN + MAX_DATA_LEN + 7)
 
 /**
  * A request consists of a method, a URI, and data. The serialized form is
- * "<method> <uri>\n" for short methods, and "<method> <uri>\n\n<data>\n\n"
- * for long methods.
+ * "<method>[ <uri>]\n[\n<data>\n\n]".
  *
  * The following list describes implemented methods in the protocol. Unless
  * otherwise noted, the only possible response status code is 200 (OK).
@@ -39,14 +44,15 @@
  */
 struct request {
     enum METHOD { GETA, RSUM, SUSP, PING, INFO, NTFY } method;
-    char uri[64];
-    char data[512];
+    int has_uri:1;
+    int has_data:1;
+    char uri[MAX_URI_LEN];
+    char data[MAX_DATA_LEN];
 };
 
 /**
  * A response consists of a status code, a message, and data. The serialized
- * form is "<status code> <message>\n" for short responses, and
- * "<status code> <message>\n\n<data>\n\n" for long responses.
+ * form is "<status code> <message>\n[\n<data>\n\n]".
  *
  * The following is a list of status codes and corresponding messages.
  *
@@ -60,8 +66,41 @@ struct request {
  */
 struct response {
     int status;
-    char message[64];
-    char data[512];
-}
+    char message[MAX_MESSAGE_LEN];
+    int has_data:1;
+    char data[MAX_DATA_LEN];
+};
+
+/**
+ * Parse the serialized request into the request structure.
+ *
+ * Return 0 on success, -1 on error.
+ */
+int
+parse_request(const char *, struct request *);
+
+/**
+ * Parse the serialized response into the response structure.
+ *
+ * Return 0 on success, -1 on error.
+ */
+int
+parse_response(const char *, struct response *);
+
+/**
+ * Serialize the request into the character buffer.
+ *
+ * Return 0 on success, -1 on error.
+ */
+int
+serialize_request(const struct request *, char *);
+
+/**
+ * Serialize the response into the character buffer.
+ *
+ * Return 0 on success, -1 on error.
+ */
+int
+serialize_response(const struct response *, char *);
 
 #endif /* PROTOCOL_H */
