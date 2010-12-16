ac76469d0282ee23d926122b610e01d48653b8b4
Chungmin Lee <lemonedo@gmail.com>
2010-12-16 10:11:42 +0900

    minor refactorings
    * renamed sock_name to sock_file
    * replaced strerror()s with %m (Glibc extensions)

--- server/proxy_src/main.c
+++ server/proxy_src/main.c
@@ -22,9 +22,9 @@
 
 #define QLEN 10
 
-int sock;  // file descriptor for the socket
-const char *sock_name;  // filename for the socket
-const char *pid_file;  // filename of the pid file
+int sock;  // file descriptor for the Unix domain socket for the web UI
+const char *sock_file;  // filename for the Unix domain socket for the web UI
+const char *pid_file;   // filename of the pid file
 
 //
 // TODO communicate with the agent
@@ -32,7 +32,7 @@ const char *pid_file;  // filename of the pid file
 
 // Make a Unix domain socket to communicate with the web UI
 static int
-bind_and_listen(const char *name, struct sockaddr_un *, size_t *);
+bind_and_listen(const char *sock_file, struct sockaddr_un *, size_t *);
 
 // Communicate with the web UI
 static void
@@ -55,53 +55,54 @@ main()
     //
     // TODO getopt
     //
-    sock_name = DEFAULT_SOCKET;
+    sock_file = DEFAULT_SOCKET;
     pid_file = DEFAULT_PID_FILE;
 
     // Make the process a daemon
     daemonize(program_invocation_short_name);
 
-    sock = bind_and_listen(sock_name, &addr, &addr_len);
+    sock = bind_and_listen(sock_file, &addr, &addr_len);
 
     if (write_pid(pid_file) < 0)
-        syslog(LOG_WARNING, "can't write PID file to %s: %s", pid_file,
-            strerror(errno));
+        syslog(LOG_WARNING, "can't write PID file to %s: %m", pid_file);
     if (setuid(getuid()) < 0)
-        syslog(LOG_WARNING, "can't drop the root privileges: %s",
-            strerror(errno));
+        syslog(LOG_WARNING, "can't drop the root privileges: %m");
     if (register_signal_handler(SIGTERM, sigterm) < 0)
-        syslog(LOG_WARNING, "can't catch SIGTERM: %s", strerror(errno));
+        syslog(LOG_WARNING, "can't catch SIGTERM: %m");
     handle_connections(sock, &addr, &addr_len);
 
     return 0;
 }
 
 static int
-bind_and_listen(const char *name, struct sockaddr_un *addr, size_t *addr_len)
+bind_and_listen(const char *sock_file, struct sockaddr_un *addr, size_t *addr_len)
 {
-    int sock, size;
+    int sock, n;
 
     sock = socket(PF_UNIX, SOCK_STREAM, 0);
     if (sock < 0) {
-        syslog(LOG_ERR, "can't create a socket: %s", strerror(errno));
+        syslog(LOG_ERR, "can't create a socket: %m");
         exit(1);
     }
 
-    if (unlink(name) < 0 && errno != ENOENT) {
-        syslog(LOG_ERR, "can't unlink %s: %s", name, strerror(errno));
+    if (unlink(sock_file) < 0 && errno != ENOENT) {
+        syslog(LOG_ERR, "can't unlink %s: %m", sock_file);
         exit(1);
     }
     addr->sun_family = AF_UNIX;
-    size = snprintf(addr->sun_path, sizeof(addr->sun_path), "%s", name);
-    *addr_len = offsetof(struct sockaddr_un, sun_path) + (size_t) size;
+    n = snprintf(addr->sun_path, sizeof(addr->sun_path), "%s", sock_file);
+    if (n < 0 || (int) sizeof(addr->sun_path) < n) {
+        syslog(LOG_ERR, "socket path too long");
+        exit(1);
+    }
+    *addr_len = offsetof(struct sockaddr_un, sun_path) + (size_t) n;
 
     if (bind(sock, (struct sockaddr *) addr, *addr_len) < 0) {
-        syslog(LOG_ERR, "can't bind the socket %s: %s", name, strerror(errno));
+        syslog(LOG_ERR, "can't bind the socket %s: %m", sock_file);
         exit(1);
     }
     if (listen(sock, QLEN) < 0) {
-        syslog(LOG_ERR, "can't listen on the socket %s: %s", name,
-            strerror(errno));
+        syslog(LOG_ERR, "can't listen on the socket %s: %m", sock_file);
         exit(1);
     }
 
@@ -112,27 +113,26 @@ static void
 handle_connections(int sock, struct sockaddr_un *addr, size_t *addr_len)
 {
     static char buffer[4096];
-    int conn, nbytes;
+    int conn, n;
 
     while (1) {
         conn = accept(sock, (struct sockaddr *) addr, addr_len);
         if (conn < 0) {
-            syslog(LOG_WARNING, "can't accept: %s", strerror(errno));
+            syslog(LOG_WARNING, "can't accept: %m");
             continue;  // ...?
         }
-        while ((nbytes = read(conn, buffer, sizeof(buffer))) > 0) {
+        while ((n = read(conn, buffer, sizeof(buffer))) > 0) {
             //
             // TODO impl
             //
             // simply echo for now
-            if (write(conn, buffer, nbytes) < 0) {
-                syslog(LOG_WARNING, "can't write: %s", strerror(errno));
+            if (write(conn, buffer, n) != n) {
+                syslog(LOG_WARNING, "can't write: %m");
                 break;
             }
         }
         if (close(conn) < 0)
-            syslog(LOG_WARNING, "can't close the connection: %s",
-                strerror(errno));
+            syslog(LOG_WARNING, "can't close the connection: %m");
     }
 }
 
@@ -142,10 +142,10 @@ sigterm(int signum)
     (void) signum;  // unused
     syslog(LOG_INFO, "got SIGTERM; exiting");
     if (close(sock) < 0)
-        syslog(LOG_ERR, "can't close the socket: %s", strerror(errno));
-    if (unlink(sock_name) < 0)
-        syslog(LOG_ERR, "can't unlink %s: %s", sock_name, strerror(errno));
+        syslog(LOG_ERR, "can't close the socket: %m");
+    if (unlink(sock_file) < 0)
+        syslog(LOG_ERR, "can't unlink %s: %m", sock_file);
     if (unlink(pid_file) < 0)
-        syslog(LOG_ERR, "can't unlink %s: %s", pid_file, strerror(errno));
+        syslog(LOG_ERR, "can't unlink %s: %m", pid_file);
     exit(2);
 }
