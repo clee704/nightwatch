8c30798b56a19c358bc4b458003f04526722fa57
pwh620 <pwh620@gmail.com>
2010-12-19 19:21:21 +0900

    added ifndef

--- commons/src/poison.h
+++ commons/src/poison.h
@@ -1,4 +1,8 @@
+#ifndef POISON_H
+#define POISON_H
 struct in_addr;
 struct ether_addr;
 
 int send_poison_packet(struct in_addr *ip_addr,struct ether_addr *mac_addr, const char *ifname_in) ;
+
+#endif
